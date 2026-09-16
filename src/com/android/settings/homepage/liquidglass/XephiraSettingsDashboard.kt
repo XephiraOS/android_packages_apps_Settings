ca8af144f800cf6f7ca4f79c82224c0bcfbe57d8
phhgsi <ajay24ce008@satiengg.in>
2026-09-16 06:17:12 +0530

    Settings: Fix unresolved reference to background in XephiraSettingsDashboard
    Add missing androidx.compose.foundation.background import.

--- src/com/android/settings/homepage/liquidglass/XephiraSettingsDashboard.kt
+++ src/com/android/settings/homepage/liquidglass/XephiraSettingsDashboard.kt
@@ -28,6 +28,7 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
